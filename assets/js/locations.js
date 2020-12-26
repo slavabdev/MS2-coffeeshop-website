7a2d28b8e330edfac842b546c55db5cfd9ebff7a
slavabdev <slav.balykov@gmail.com>
2020-12-26 18:17:34 +0000

    add map markers listeners

--- assets/js/locations.js
+++ assets/js/locations.js
@@ -1,4 +1,4 @@
-
+let map;
 
 function initMap() { 
 let mapCenter= {lat: 51.898669, lng: -8.403624};
@@ -8,21 +8,29 @@ let locations = [
 ['Cork', 51.895301, -8.471848 ],
 ['Little Island', 51.904308, -8.357043]
 ];
- let map = new google.maps.Map(document.getElementById("map"), {
+map = new google.maps.Map(document.getElementById("map"), {
     center: mapCenter,
     zoom:12,
   });
 
+$(".location-button").click(function() {
+  console.log(this.dataset.lat+","+this.dataset.lng);
+  map.setCenter(new google.maps.LatLng(this.dataset.lat, this.dataset.lng));
+  map.setZoom(15);
+  });  
+
   let infowindow = new google.maps.InfoWindow({});
 
   let marker, count;
-
+  let bounds = new google.maps.LatLngBounds();
   for (count = 0; count<locations.length; count++) {
        marker = new google.maps.Marker ({
        position: new google.maps.LatLng(locations[count][1], locations[count][2]),
        map: map,
        animation: google.maps.Animation.DROP
   });
+
+bounds.extend(marker.getPosition());
   
 google.maps.event.addListener(marker, 'click', (function (marker, count) {
       return function () {
@@ -31,13 +39,8 @@ google.maps.event.addListener(marker, 'click', (function (marker, count) {
       }
     })(marker, count));
   }
+map.fitBounds(bounds);
+  
 }
 
-google.maps.event.addListener(map, 'zoom_changed', function() {
-    zoomLevel = map.getZoom();
-    if (zoomLevel >= minFTZoomLevel) {
-        FTlayer.setMap(map);
-    } else {
-        FTlayer.setMap(null);
-    }
-});
+
