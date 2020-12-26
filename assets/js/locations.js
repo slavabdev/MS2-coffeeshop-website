9f3dcdae9b5b4f7d6d7e9bfbe523d94291f9fa4e
slavabdev <slav.balykov@gmail.com>
2020-12-26 15:06:13 +0000

    added markers on map

--- assets/js/locations.js
+++ assets/js/locations.js
@@ -1,8 +1,43 @@
-let map;
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+
+function initMap() { 
+let mapCenter= {lat: 51.898669, lng: -8.403624};
+
+let locations = [
+['Blackrock', 51.898669, -8.403624 ],
+['Cork', 51.895301, -8.471848 ],
+['Little Island', 51.904308, -8.357043]
+];
+ let map = new google.maps.Map(document.getElementById("map"), {
+    center: mapCenter,
+    zoom:12,
   });
+
+  let infowindow = new google.maps.InfoWindow({});
+
+  let marker, count;
+
+  for (count = 0; count<locations.length; count++) {
+       marker = new google.maps.Marker ({
+       position: new google.maps.LatLng(locations[count][1], locations[count][2]),
+       map: map,
+       animation: google.maps.Animation.DROP
+  });
+  
+google.maps.event.addListener(marker, 'click', (function (marker, count) {
+      return function () {
+        infowindow.setContent(locations[count][0]);
+        infowindow.open(map, marker);
+      }
+    })(marker, count));
+  }
 }
+
+google.maps.event.addListener(map, 'zoom_changed', function() {
+    zoomLevel = map.getZoom();
+    if (zoomLevel >= minFTZoomLevel) {
+        FTlayer.setMap(map);
+    } else {
+        FTlayer.setMap(null);
+    }
+});
