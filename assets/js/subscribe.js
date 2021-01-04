60f00c1572fd737ce301c708c53fed0024deda4f
slavabdev <slav.balykov@gmail.com>
2021-01-04 12:31:14 +0000

    updated blocks in subscription section

--- assets/js/subscribe.js
+++ assets/js/subscribe.js
@@ -1,35 +1,15 @@
-$('#myModal').on('show.bs.modal', function (e) {
-var button = e.relatedTarget;
-if($("#submitForm").val().length==0) {
-e.stopPropegation();
-}  
-});
-
-
-
-
-
-
-/*let cupCup = function cup () {
-    let cups = document.getElementById('cups').value;
+function toggleFunction() {
+  let opt = document.getElementById("subscribe-opt");
+  if (opt.style.display === "block") {
+    opt.style.display = "none";
+  } else {
+    opt.style.display = "block";
+  }
 }
 
-let brewBrew = function brew (){
- let brewStyle = document.querySelectorAll('input[name="brewChoice"]')
- let selectedBrew;
- for (let brew of brewStyle){
-     if(brew.checked){
-         selectedBrew=brew.value;
-     }
- }
 
-
-
- break
+function addtext(content) {
+    let text = document.getElementById('form-text');
+    text.value+=content;
 }
 
-btn.onclick = function calc() {
-   let result = cupCup * brewBrew / 250;
-     document.getElementById('my-total').innerHTML = (`asdasdasd ${result}`);
-}
-*/
